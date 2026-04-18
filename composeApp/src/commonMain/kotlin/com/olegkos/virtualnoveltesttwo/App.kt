bf9c3b79b30e9235d0b6b7035b20bb6d90df00b1
OlegKostin <olegkostin89@yandex.ru>
2026-04-18 10:23:56 +0300

    refactored card image at ui

--- composeApp/src/commonMain/kotlin/com/olegkos/virtualnoveltesttwo/App.kt
+++ composeApp/src/commonMain/kotlin/com/olegkos/virtualnoveltesttwo/App.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -148,12 +149,14 @@ fun App(viewModel: GameViewModel = koinViewModel()) {
           painter = it,
           contentDescription = null,
           modifier = Modifier
-            .size(200.dp)
+            .height(maxHeight * 0.8f)
+            .fillMaxWidth()
             .align(Alignment.Center),
           contentScale = ContentScale.Fit
         )
       }
     }
+
 
     characters.forEach { char ->
       val painter = rememberPainter(viewModel.assets.character(char.image), viewModel.reader)
